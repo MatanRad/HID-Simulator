c2949b11fe8c45b0b85289bbe9d7211d3d1b302a
Matan Radomski <MatanRad@users.noreply.github.com>
2023-04-29 04:05:44 +0300

    Added support for readfile & writefile with custom bsod command!

--- KMDF Driver1/driver.c
+++ KMDF Driver1/driver.c
@@ -10,6 +10,9 @@ EVT_WDF_DRIVER_DEVICE_ADD KmdfHelloWorldEvtDeviceAdd;
 #define DOS_DEVICE_NAME  L"\\DosDevices\\MyDevice"
 DECLARE_CONST_UNICODE_STRING(dosDeviceName, DOS_DEVICE_NAME);
 
+unsigned char g_buf[4096] = { 0 };
+unsigned long g_buf_len;
+
 WDFQUEUE  g_queue = NULL;
 
 
@@ -40,20 +43,66 @@ void KmdfIoQueueIoWrite(WDFQUEUE Queue, WDFREQUEST Request, size_t Length) {
 
     status = WdfRequestRetrieveInputMemory(Request, &memory);
     if (!NT_SUCCESS(status)) {
-        TRACE(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[MY_DRIVER] Error retriving memory: %l!\n", status);
+        TRACE(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[MY_DRIVER] Error retriving memory on write: %l!\n", status);
+        WdfRequestComplete(Request, status);
         return;
     }
 
     buffer = WdfMemoryGetBuffer(memory, &bufferLength);
     if (buffer == NULL) {
-        TRACE(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[MY_DRIVER] Error retriving memory: %l!\n", status);
+        TRACE(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[MY_DRIVER] Error retriving buffer on write: %l!\n", status);
+        WdfRequestComplete(Request, status);
         return;
     }
 
     // Print the buffer to the debug console
     TRACE(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[MY_DRIVER] Message: %s!\n", (PCSTR)buffer);
 
+    if (bufferLength > sizeof(g_buf) || bufferLength == 0) {
+        g_buf_len = 0;
+    } else {
+        g_buf_len = (unsigned long)bufferLength;
+        memcpy(g_buf, buffer, g_buf_len);
+    }
+
+    if (g_buf_len == 3 && g_buf[0] == 'b' && g_buf[1] == 'y' && g_buf[2] == 'e') {
+        // Time to do a pro gamer move!
+        volatile PWDFDEVICE_INIT a = NULL;
+        WdfDeviceInitSetIoType(a, WdfDeviceIoBuffered);
+    }
+
     WdfRequestComplete(Request, status);
+}
+
+void KmdfIoQueueIoRead(WDFQUEUE Queue, WDFREQUEST Request, size_t Length) {
+    NTSTATUS status;
+    WDFMEMORY memory;
+    PVOID buffer;
+    size_t bufferLength;
+
+    UNREFERENCED_PARAMETER(Queue);
+    UNREFERENCED_PARAMETER(Length);
+
+    status = WdfRequestRetrieveOutputMemory(Request, &memory);
+    if (!NT_SUCCESS(status)) {
+        TRACE(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[MY_DRIVER] Error retriving memory on read: %l!\n", status);
+        WdfRequestComplete(Request, status);
+        return;
+    }
+
+    buffer = WdfMemoryGetBuffer(memory, &bufferLength);
+    if (buffer == NULL) {
+        TRACE(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[MY_DRIVER] Error retriving buffer on write: %l!\n", status);
+        WdfRequestComplete(Request, status);
+        return;
+    }
+
+    if (g_buf_len > 0) {
+        memcpy_s(buffer, bufferLength, g_buf, g_buf_len);
+        g_buf_len = 0;
+    }
+    
+    WdfRequestCompleteWithInformation(Request, status, bufferLength);
 }
 
 NTSTATUS KmdfHelloWorldEvtDeviceAdd(WDFDRIVER Driver, PWDFDEVICE_INIT DeviceInit) {
@@ -96,6 +145,7 @@ NTSTATUS KmdfHelloWorldEvtDeviceAdd(WDFDRIVER Driver, PWDFDEVICE_INIT DeviceInit
     // IO Queue
     WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE(&ioQueueConfig, WdfIoQueueDispatchSequential);
     ioQueueConfig.EvtIoWrite = KmdfIoQueueIoWrite;
+    ioQueueConfig.EvtIoRead = KmdfIoQueueIoRead;
 
     status = WdfIoQueueCreate(hDevice, &ioQueueConfig, WDF_NO_OBJECT_ATTRIBUTES, &g_queue);
     if (!NT_SUCCESS(status)) {
