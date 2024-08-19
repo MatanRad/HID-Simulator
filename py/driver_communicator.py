2ab3efb6eeee662531b43cbb2d3b7d0e91381f9b
Matan Radomski <MatanRad@users.noreply.github.com>
2024-08-19 17:47:00 +0300

    Moved Driver Manager

--- py/driver_communicator.py
+++ py/driver_communicator.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from commands import SetDeviceCommand, ClearDeviceCommand, ByeCommand, SendInputCommand
 import socket
 import win32file
 
@@ -69,4 +70,31 @@ class DirectDriverCommunicator(BaseDriverCommunicator):
 
     def receive(self):
         raise NotImplementedError()
+
+
+class DriverManager(object):
+    def __init__(self, communicator: BaseDriverCommunicator):
+        self._communicator = communicator
+
+    def set_device(self, report_desc: bytes):
+        set_dev_cmd = SetDeviceCommand.build(dict(dev=dict(report_desc=report_desc)))
+        self._communicator.send(set_dev_cmd)
+
+    def clear_device(self):
+        clear_dev_cmd = ClearDeviceCommand.build({})
+        self._communicator.send(clear_dev_cmd)
     
+    def bye(self):
+        bye_cmd = ByeCommand.build({})
+        self._communicator.send(bye_cmd)
+
+    def send_input(self, report: bytes, report_id: int = 0):
+        send_cmd = SendInputCommand.build(dict(report=report, report_id=report_id))
+        self._communicator.send(send_cmd)
+
+    def __enter__(self):
+        self._communicator.__enter__()
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._communicator.__exit__(exc_type, exc_val, exc_tb)
--- py/main.py
+++ py/main.py
@@ -1,4 +1,4 @@
-from driver_communicator import BaseDriverCommunicator, SocketDriverCommunicator, DirectDriverCommunicator
+from driver_communicator import DriverManager, SocketDriverCommunicator, DirectDriverCommunicator
 from commands import SetDeviceCommand, ClearDeviceCommand, ByeCommand, SendInputCommand
 
 JOYSTICK_REPORT = bytes([
@@ -59,35 +59,8 @@ MODIFIED_JOYSTICK_REPORT = bytes([
 	0xc0                 # End Collection                                     */
 ])
 
-class DriverManager(object):
-    def __init__(self, communicator: BaseDriverCommunicator):
-        self._communicator = communicator
-
-    def set_device(self, report_desc: bytes):
-        set_dev_cmd = SetDeviceCommand.build(dict(dev=dict(report_desc=report_desc)))
-        self._communicator.send(set_dev_cmd)
-
-    def clear_device(self):
-        clear_dev_cmd = ClearDeviceCommand.build({})
-        self._communicator.send(clear_dev_cmd)
-    
-    def bye(self):
-        bye_cmd = ByeCommand.build({})
-        self._communicator.send(bye_cmd)
-
-    def send_input(self, report: bytes, report_id: int = 0):
-        send_cmd = SendInputCommand.build(dict(report=report, report_id=report_id))
-        self._communicator.send(send_cmd)
-
-    def __enter__(self):
-        self._communicator.__enter__()
-        return self
-    
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self._communicator.__exit__(exc_type, exc_val, exc_tb)
-
 
 if __name__ == "__main__":
-    with DriverManager(SocketDriverCommunicator("192.168.40.128")) as d:
+    with DriverManager(DirectDriverCommunicator()) as d:
         import IPython
         IPython.embed()
